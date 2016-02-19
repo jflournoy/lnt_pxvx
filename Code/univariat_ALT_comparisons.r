8f07cea355d69477824cc5fa49b3b37a67862a76
John Flournoy <uoflournoy@gmail.com>
2016-02-19 11:23:10 -0800

    output diagram and tables

--- Code/univariat_ALT_comparisons.r
+++ Code/univariat_ALT_comparisons.r
@@ -1,14 +1,43 @@
+#'---
+#'title: "Life and Time - PxVx - Base Model Comparisons"
+#'output:
+#'  html_document
+#'---
+
+#+ echo=F
 library(MplusAutomation)
 library(knitr)
 library(dplyr)
 library(tidyr)
 library(semPlot)
+library(printr)
+opts_chunk$set(echo=F)
+
+#'
+#' **Template for model comparisons:**
+#'
+#'![](initial_model.png "Template for model comparisons")
+#'
+#' The above diagram captures all 4 models we wish to compare. Ignoring the 
+#' blue portion, we have an intercept-only ALT model. We can decide whether
+#' the auto-regressive paths improve fit by comparing a model where `AR` is
+#' freely estimated to a model where `AR` is constrained to be 0 -- the
+#' *AR_Int* versus *NoAR_Int* models in the tables below. This comparison
+#' results in a difference of 1 parameter between the models.
+#'
+#' We can also include a latent slope (the blue portion, above) and compare
+#' model fit between an intercept only, and intercept + slope model (with 
+#' `AR` free or constrained). Adding the slope
+#' gives us 4 new parameters to estimate: The slope mean, varriance, and 
+#' covarriance with the intercept and initial status indicator (`a X`). These
+#' two-parameter latent growth models are postfixed with *Lin* below (e.g., 
+#' *NoAR_Lin*).
+#'
 
 # Set working directory to that which contains Code, Data, etc
 #setwd('E:/Projects/LnT_Values/')
-setwd('/mnt/328392813EB3641A/Projects/lnt_pxvx/')
 
-saveUniFN<-'Rez/uniMods.RData'
+saveUniFN<-'../Rez/uniMods.RData'
 
 # # Create Models
 # createModels('code/PxVx_UniTemplate.inp')
@@ -25,8 +54,6 @@ saveUniFN<-'Rez/uniMods.RData'
 
 load(saveUniFN)
 
-str(uniModelOut_df[1,1][[1]][[1]]$summaries, max.level=2)
-
 summaries <- uniModelOut_df %>% rowwise %>%
   do({
     aSummary <- .[[1]]$summaries
@@ -36,8 +63,17 @@ summaries <- uniModelOut_df %>% rowwise %>%
           c('sample', 'variable', 'modelType'),
           'PxVx Univariate - (\\w+) ([\\w_]+) ([\\w_]+)')
 
-#This method for comparison of models is taken from
-# https://www.statmodel.com/chidiff.shtml
+#'
+#' The below tables test fit improvements between models with and without 
+#' the `AR` path constrained to 0, and with and without the latent slope.
+#' because we use the `tscores` command in MPlus, the MLR estimator is used,
+#' which requires an adjustment to the -2\*Log Likelhood values before their
+#' difference is computed. For more information on this adjustment, see 
+#' the [MPlus website]( https://www.statmodel.com/chidiff.shtml) or the
+#' [UCLA website](and http://www.ats.ucla.edu/stat/mplus/faq/s_b_chi2.htm).
+#' The *p*-values are calculated for the adjusted -2\*LL difference with
+#' *df* equal to the difference in number of parameters estimated.
+#'
 
 modelComparisonResults <- summaries %>% group_by(sample, variable) %>%
   do({
@@ -76,4 +112,22 @@ modelComparisonResults <- summaries %>% group_by(sample, variable) %>%
 		     PVal=PVals)
   })
 
-kable(modelComparisonResults)
+sampleNames <- c(Col='College', Nat='National', `Inf`='Informant')
+
+#+ results='asis'
+theTables <- modelComparisonResults %>% group_by(sample, variable) %>%
+	do({
+		caption <- paste0(sampleNames[.$sample[[1]]], ' Sample, ',
+				  .$variable[[1]])
+		dataForTable <- select(., model1, model2, LLm1, LLm2, nPm1, nPm2, 
+				       TRd, df, PVal)
+		theTable <- kable(dataForTable,
+		      		  digits=2,
+		      		  caption=caption,
+		      		  col.names=c('Model 1', 'Model 2', 'LL M1', 'LL M2', 
+						    'Params M1', 'Params M2', 
+						    'Adj -2*LL $\\Delta$', '*df*', '*p*'))
+		print(theTable)
+		data_frame(Table=list(theTable))
+	})
+
