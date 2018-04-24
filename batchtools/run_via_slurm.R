b73b006c02695d219d97b0686f5c5ebe397f7670
John Flournoy <uoflournoy@gmail.com>
2018-04-24 13:40:21 -0400

    batch helpers

---
+++ batchtools/run_via_slurm.R
@@ -0,0 +1,9 @@
+library(future)
+library(future.batchtools)
+
+plan(batchtools_slurm,
+     template = '/home/flournoy/otherhome/code/lnt_pxvx/batchtools.slurm.tmpl',
+     resources = list(ncpus = 1, walltime = 60*24-1, memory = '1G',
+                      partitions = 'short,fat,long,longfat,preempt'))
+
+rmarkdown::render("national_sample_values_measurement.Rmd")
