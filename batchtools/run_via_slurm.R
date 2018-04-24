652ab9cc9e1f28c57d153715b4b50a619eaf7c7f
John Flournoy <uoflournoy@gmail.com>
2018-04-24 17:28:52 -0400

    mem

--- batchtools/run_via_slurm.R
+++ batchtools/run_via_slurm.R
@@ -3,7 +3,7 @@ library(future.batchtools)
 
 plan(batchtools_slurm,
      template = '/home/flournoy/otherhome/code/lnt_pxvx/batchtools/batchtools.slurm.tmpl',
-     resources = list(ncpus = 1, walltime = 60*24-1, memory = '500M',
+     resources = list(ncpus = 1, walltime = 60*24-1, memory = '100M',
                       partitions = 'short,fat,long,longfat,preempt'))
 
 rmarkdown::render("national_sample_values_measurement.Rmd")
